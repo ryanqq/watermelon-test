1d68f256981cba73b27320ed1029727839027f59
zengwei81@gmail.com <zengwei81@gmail.com@bdaca81d-25f6-00a4-625a-03fdb058ac69>
2012-06-20 09:06:17 +0000

    svm

--- trunk/WaterMelonJava/src/ntu/sec/wm/WMClassify.java
+++ trunk/WaterMelonJava/src/ntu/sec/wm/WMClassify.java
@@ -5,21 +5,47 @@ import java.util.Vector;
 import ntu.sec.wm.ft.TransformAudioSignal;
 
 import ntu.sec.wm.preprocessing.Preprocessing;
+import ntu.sec.wm.svm.Classify;
+import ntu.sec.wm.svm.Train;
 
 public class WMClassify {
 	Vector<float[]> samples;
-	public static void main(String[] args){
+	private String wav = ".wav";
+	private String csv = ".csv";
+	
+	String input_wm = "wm/Far/GOOD_2012_6_12_22_55_17";
+	String input_chair = "wm/Bad/BAD_2012_6_20_14_37_31";
+
+//	String test = "wm/Bad/BAD_2012_6_20_14_37_22";
+	String test = "wm/Far/GOOD_2012_6_12_22_55_17";
+
+	public static void main(String[] args) {
 		(new WMClassify()).run();
 	}
-	
-	public void run(){
-		Preprocessing pre = new Preprocessing();
+
+	public void run() {
+		Preprocessing pre_wm = new Preprocessing(input_wm+wav);
+		pre_wm.run();
+		TransformAudioSignal fft_wm = new TransformAudioSignal(
+				pre_wm.getFrames());
+
+		Preprocessing pre_chair = new Preprocessing(input_chair+wav);
+		pre_chair.run();
+		TransformAudioSignal fft_chair = new TransformAudioSignal(
+				pre_chair.getFrames());
+
+		Preprocessing pre_test = new Preprocessing(input_wm+wav);
+		pre_test.run();
+		TransformAudioSignal fft_test = new TransformAudioSignal(
+				pre_test.getFrames());
+
+//		fft_wm.write_to_csv_file(input_wm+csv);
+//		fft_chair.write_to_csv_file(input_chair+csv);
+//		fft_test.write_to_csv_file(test+csv);
 		
-		pre.run();
-		
-		samples = pre.getFrames();
-		
-		TransformAudioSignal fft = new TransformAudioSignal(samples);
-		fft.write_to_csv_file("wm/results/Far/GOOD_2012_6_12_22_25_24.csv");
+		Train train = new Train(fft_wm.getFFT(), fft_chair.getFFT());
+		train.run();
+		Classify classify = new Classify(fft_test.getFFT().get(0));
+		classify.run();
 	}
 }
--- trunk/WaterMelonJava/src/ntu/sec/wm/svm/Train.java
+++ trunk/WaterMelonJava/src/ntu/sec/wm/svm/Train.java
@@ -1,5 +1,95 @@
 package ntu.sec.wm.svm;
 
-public class Train {
+import java.io.IOException;
+import java.util.Vector;
 
+import libsvm.svm;
+import libsvm.svm_model;
+import libsvm.svm_node;
+import libsvm.svm_parameter;
+import libsvm.svm_problem;
+
+public class Train {
+	private svm_parameter param;
+	private svm_problem prob;
+	private svm_model model;
+	private String model_file = "wm/model";
+	private Vector<float[]> positive;
+	private Vector<float[]> negtive;
+	
+	//the period of features
+	private int min = 100;
+	private int max = 350;
+	
+	public Train(Vector<float[]> positive, Vector<float[]> negtive){
+		this.positive = positive;
+		this.negtive = negtive;
+	}
+
+	public void run(){
+		assign_parameter();
+		assign_problem();
+		
+		model = svm.svm_train(prob, param);
+		try {
+			svm.svm_save_model(model_file, model);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void assign_problem(){
+		prob = new svm_problem();
+		prob.l = positive.size() + negtive.size();
+		prob.x = new svm_node[prob.l][];
+		prob.y = new double[prob.l];
+		
+		for(int i = 0; i < positive.size(); i ++){
+			float[] in = positive.get(i);
+			svm_node[] x = new svm_node[max - min];
+			for(int j = 0; j < x.length; j++)
+			{
+				x[j] = new svm_node();
+				x[j].index = j;
+				x[j].value = in[min+j];
+			}
+			
+			prob.x[i] = x;
+			prob.y[i] = 1;
+		}		
+		
+		for(int i = 0; i < negtive.size(); i ++){
+			float[] in = negtive.get(i);
+			svm_node[] x = new svm_node[max - min];
+			for(int j = 0; j < x.length; j++)
+			{
+				x[j] = new svm_node();
+				x[j].index = j;
+				x[j].value = in[min+j];
+			}
+			
+			prob.x[i+positive.size()] = x;
+			prob.y[i+positive.size()] = -1;
+		}		
+	}
+	
+	private void assign_parameter(){
+		param = new svm_parameter();
+		// default values
+		param.svm_type = svm_parameter.C_SVC;
+		param.kernel_type = svm_parameter.RBF;
+		param.degree = 3;
+		param.gamma = 0;	// 1/num_features
+		param.coef0 = 0;
+		param.nu = 0.5;
+		param.cache_size = 100;
+		param.C = 1;
+		param.eps = 1e-3;
+		param.p = 0.1;
+		param.shrinking = 1;
+		param.probability = 0;
+		param.nr_weight = 0;
+		param.weight_label = new int[0];
+		param.weight = new double[0];
+	}
 }
